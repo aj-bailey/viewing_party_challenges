57191a69265c1d51e09dcf61e29aabe34a4a18f9
Adam Bailey <ab67319@gmail.com>
2023-04-03 17:59:56 -0600

    Revert "refactor: fix issues"
    This reverts commit 461d59a08f73148801995e998be3d9d9ad9fd01d.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,12 +26,12 @@ class UsersController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user.authenticate(params[:password])
-      # session[:user_id] = user.id
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.email}"
       redirect_to user_path(user)
     else
-      flash.now[:alert] = "Sorry, your credentials are bad"
-      render :login_form, status: 400
+      flash[:error] = "Sorry, your credentials are bad"
+      render :login_form
     end
   end
 
