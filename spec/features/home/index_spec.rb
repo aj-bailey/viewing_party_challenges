22ac83a66d508c67bf45ac32ba543c8f1d52a750
Adam Bailey <ab67319@gmail.com>
2023-04-04 15:03:01 -0600

    feat: create user story #2

--- spec/features/home/index_spec.rb
+++ spec/features/home/index_spec.rb
@@ -6,36 +6,24 @@ RSpec.describe 'home page, index', type: :feature do
     @adam = User.create!(name: "Adam", email: "adam@aol.com", password: "test123", password_confirmation: "test123")
     @james = User.create!(name: "James", email: "james@gmail.com", password: "test123", password_confirmation: "test123")
     @mike = User.create!(name: "Mike", email: "mike@hotmail.com", password: "test123", password_confirmation: "test123")
-    visit "/"
+    visit root_path
   end
 
   describe "as a visitor" do
     describe "when I visit the home page" do
       it "cannot see the section of the page that lists existing users" do
-        expect(page).to_not have_link("adam@aol.com", href: user_path(@adam))
-        expect(page).to_not have_link("james@gmail.com", href: user_path(@james))
-        expect(page).to_not have_link("mike@hotmail.com", href: user_path(@mike))
+        expect(page).to_not have_content("adam@aol.com")
+        expect(page).to_not have_content("james@gmail.com")
+        expect(page).to_not have_content("mike@hotmail.com")
       end
-    end
-  end
 
-  describe " as a user" do
-    describe " when I visit home page" do
-      it " I see a button to create a new user" do
+      it "can see a button to create a new user" do
         expect(page).to have_button("Create new user")
 
         click_button "Create new user"
 
         expect(current_path).to eq(register_path)
       end
-
-      # it " Has a list of existing Users which links to the users dashboard" do
-      #   within("#existing_users"){
-      #     expect(page).to have_link("adam@aol.com", href: user_path(@adam))
-      #     expect(page).to have_link("james@gmail.com", href: user_path(@james))
-      #     expect(page).to have_link("mike@hotmail.com", href: user_path(@mike))
-      #   }
-      # end
 
       it "has a link to log in" do
         expect(page).to have_link("Login", href: login_path)
@@ -47,16 +35,36 @@ RSpec.describe 'home page, index', type: :feature do
     end
   end
 
-  describe "As a logged in user" do
-    it "can no longer see a link to log in or create an account when visiting landing page" do
-      user = User.create!(name: "mike", email: "mike@aol.com", password: "password123")
+  describe " as a user" do
+    describe " when I visit home page" do
+      it "has a list of existing users emails without links to their show page" do
+        user = User.create!(name: "mike", email: "mike@aol.com", password: "password123")
       
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit root_path
-      
-      expect(page).to_not have_link("Login")
-      expect(page).to have_link("Logout")
+        visit root_path
+
+        within("#existing_users") {
+          expect(page).to have_content("adam@aol.com")
+          expect(page).to have_content("james@gmail.com")
+          expect(page).to have_content("mike@hotmail.com")
+
+          expect(page).to_not have_link("adam@aol.com", href: user_path(@adam))
+          expect(page).to_not have_link("james@gmail.com", href: user_path(@james))
+          expect(page).to_not have_link("mike@hotmail.com", href: user_path(@mike))
+        }
+      end
+
+      it "can no longer see a link to log in or create an account when visiting landing page" do
+        user = User.create!(name: "mike", email: "mike@aol.com", password: "password123")
+        
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  
+        visit root_path
+        
+        expect(page).to_not have_link("Login")
+        expect(page).to have_link("Logout")
+      end
     end
   end
 end
